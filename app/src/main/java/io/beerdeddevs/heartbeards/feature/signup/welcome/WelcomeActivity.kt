af30499aa302ee7ad12c62e8fea14677dec231dd
Daniel Hartwich <daniel.hartwich@xing.com>
2017-11-26 11:19:19 +0100

    Include firebase auth ui

---
+++ app/src/main/java/io/beerdeddevs/heartbeards/feature/signup/welcome/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package io.beerdeddevs.heartbeards.feature.signup.welcome
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.beerdeddevs.heartbeards.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
