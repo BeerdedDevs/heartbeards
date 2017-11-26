945f4daaf5dbb95cec82219bf2f3b5eab71b46d1
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-11-26 13:36:30 +0100

    Use normal mode or dark mode depending on the experiment.

--- app/src/main/java/io/beerdeddevs/heartbeards/HeartBeardsApplication.kt
+++ app/src/main/java/io/beerdeddevs/heartbeards/HeartBeardsApplication.kt
@@ -3,13 +3,13 @@ package io.beerdeddevs.heartbeards
 import android.app.Activity
 import android.app.Application
 import android.support.design.widget.BottomSheetDialogFragment
+import android.support.v7.app.AppCompatDelegate
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import io.beerdeddevs.heartbeards.di.component.ApplicationComponent
 import java.util.concurrent.TimeUnit
 
 class HeartBeardsApplication : Application() {
-
     val applicationComponent by lazy { ApplicationComponent.create(this) }
 
     override fun onCreate() {
@@ -20,6 +20,8 @@ class HeartBeardsApplication : Application() {
         firebaseRemoteConfig.fetch(if (BuildConfig.DEBUG) 1 else TimeUnit.HOURS.toSeconds(12))
             .addOnSuccessListener { firebaseRemoteConfig.activateFetched() }
             .addOnFailureListener { throw it }
+
+        AppCompatDelegate.setDefaultNightMode(firebaseRemoteConfig.getLong("night_mode").toInt())
     }
 }
 
