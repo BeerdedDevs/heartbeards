73f7303b8f8b4b873393e3e7ca2d3edecb82b665
Daniel Hartwich <daniel.hartwich@xing.com>
2017-11-26 14:39:14 +0100

    add admin sdk for cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
