964dba8c5fbe6c0ef7a20dda00c51f0bdc53d9b9
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-11-26 15:03:59 +0100

    Remove WelcomeActivity.

--- app/src/main/java/io/beerdeddevs/heartbeards/feature/timeline/TimelineActivity.kt
+++ app/src/main/java/io/beerdeddevs/heartbeards/feature/timeline/TimelineActivity.kt
@@ -4,35 +4,50 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.gms.appinvite.AppInviteInvitation
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import io.beerdeddevs.heartbeards.R
 import io.beerdeddevs.heartbeards.feature.picture.choose.BottomSheetChoosePicture
-import io.beerdeddevs.heartbeards.feature.signup.welcome.WelcomeActivity
+import io.beerdeddevs.heartbeards.getComponent
+import io.beerdeddevs.heartbeards.preferences.BeardPrefs
 import io.beerdeddevs.heartbeards.timelineReference
-
+import javax.inject.Inject
 
 
 const val REQUEST_CODE_SIGN_IN = 111
 const val REQUEST_INVITE = 112
 
 class TimelineActivity : AppCompatActivity() {
+    @BindView(R.id.activityTimeline) internal lateinit var rootView: View
+
+    @Inject internal lateinit var firebaseAnalytics: FirebaseAnalytics
+    @Inject internal lateinit var beardPrefs: BeardPrefs
+
     private lateinit var adapter: TimelineAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_timeline)
         ButterKnife.bind(this)
+
+        getComponent().inject(this)
 
         val options = FirebaseRecyclerOptions.Builder<TimelineItem>()
                 .setQuery(timelineReference.orderByChild("timestamp").limitToLast(50), TimelineItem::class.java)
@@ -49,8 +64,14 @@ class TimelineActivity : AppCompatActivity() {
     @OnClick(R.id.addImageFab)
     fun addImageFabClicked() {
         if (FirebaseAuth.getInstance().currentUser == null) {
-            startActivityForResult(Intent(this@TimelineActivity, WelcomeActivity::class.java),
-                    REQUEST_CODE_SIGN_IN)
+            val intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(
+                    listOf(AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                        AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                    ))
+                .build()
+            startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
         } else {
             BottomSheetChoosePicture().show(this@TimelineActivity)
         }
@@ -69,9 +90,29 @@ class TimelineActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == REQUEST_CODE_SIGN_IN && resultCode == Activity.RESULT_OK) {
-            BottomSheetChoosePicture().show(this@TimelineActivity)
-            invalidateOptionsMenu()
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val signUpResponse = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                showSnackbar(R.string.sign_in_successful)
+                val bundle = Bundle().apply {
+                    putString("STATUS", "completed")
+                }
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SIGN_UP, bundle)
+
+                BottomSheetChoosePicture().show(this@TimelineActivity)
+                invalidateOptionsMenu()
+            } else {
+                if (signUpResponse == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled)
+                    return
+                }
+
+                when (signUpResponse.errorCode) {
+                    ErrorCodes.NO_NETWORK -> showSnackbar(R.string.no_internet_connection)
+                    ErrorCodes.UNKNOWN_ERROR -> showSnackbar(R.string.unknown_error)
+                }
+            }
         } else if (requestCode == REQUEST_INVITE && resultCode == Activity.RESULT_OK) {
             // TODO we could do something here but firebase already shows a Snackbar.
         }
@@ -109,4 +150,8 @@ class TimelineActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+
+    private fun showSnackbar(resourceId: Int) {
+        Snackbar.make(rootView, resourceId, Snackbar.LENGTH_SHORT).show()
+    }
 }
