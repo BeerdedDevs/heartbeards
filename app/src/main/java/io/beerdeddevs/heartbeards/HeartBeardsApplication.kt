0ec260c3596ac14467b7317b1409bb7bc5b71c98
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-11-26 13:18:24 +0100

    Remote config.

--- app/src/main/java/io/beerdeddevs/heartbeards/HeartBeardsApplication.kt
+++ app/src/main/java/io/beerdeddevs/heartbeards/HeartBeardsApplication.kt
@@ -3,12 +3,24 @@ package io.beerdeddevs.heartbeards
 import android.app.Activity
 import android.app.Application
 import android.support.design.widget.BottomSheetDialogFragment
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import io.beerdeddevs.heartbeards.di.component.ApplicationComponent
+import java.util.concurrent.TimeUnit
 
 class HeartBeardsApplication : Application() {
 
     val applicationComponent by lazy { ApplicationComponent.create(this) }
 
+    override fun onCreate() {
+        super.onCreate()
+
+        val firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+        firebaseRemoteConfig.setConfigSettings(FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG).build())
+        firebaseRemoteConfig.fetch(if (BuildConfig.DEBUG) 1 else TimeUnit.HOURS.toSeconds(12))
+            .addOnSuccessListener { firebaseRemoteConfig.activateFetched() }
+            .addOnFailureListener { throw it }
+    }
 }
 
 fun Activity.getComponent() = (application as HeartBeardsApplication).applicationComponent
