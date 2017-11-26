ef4bbf78991f84706f3a6c156d1dc7497acafef3
Yahya Bayramoglu <yahyabayramoglu@gmail.com>
2017-11-26 15:57:17 +0100

    Update registration ID

--- app/src/main/java/io/beerdeddevs/heartbeards/notification/MyFirebaseInstanceIDService.kt
+++ app/src/main/java/io/beerdeddevs/heartbeards/notification/MyFirebaseInstanceIDService.kt
@@ -1,5 +1,6 @@
 package io.beerdeddevs.heartbeards.notification
 
+import android.util.Log
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.iid.FirebaseInstanceIdService
 import com.google.firebase.iid.FirebaseInstanceId
@@ -9,22 +10,21 @@ class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
     override fun onTokenRefresh() {
         // Get updated InstanceID token.
         val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.e("MyInstanceIDService", "refreshedToken: $refreshedToken")
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken)
+        sendRegistrationToServer(NotificationToken(refreshedToken ?: ""))
     }
 
-    private fun sendRegistrationToServer(refreshedToken: String?) {
-        refreshedToken?.let {
-            FirebaseDatabase.getInstance()
-                    .reference
-                    .child("notificationTokens")
-                    .push()
-                    .setValue(refreshedToken)
-        }
+    private fun sendRegistrationToServer(refreshedToken: NotificationToken) {
+        FirebaseDatabase.getInstance()
+                .reference
+                .child("notificationTokens/${refreshedToken.token}")
+                .setValue(true)
     }
 
+    data class NotificationToken(val token: String)
 
 }
