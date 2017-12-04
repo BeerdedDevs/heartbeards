e94cbb4a957090d5195c073919082bdcf3a6571e
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-12-04 16:53:24 +0100

    Proxy FirebaseRemoteConfig behind Config interface. (#19)

---
+++ app/src/main/java/io/beerdeddevs/heartbeards/io/config/Config.kt
@@ -0,0 +1,14 @@
+package io.beerdeddevs.heartbeards.io.config
+
+import android.support.annotation.ColorInt
+import android.support.v7.app.AppCompatDelegate.NightMode
+
+interface Config {
+  fun fetch()
+
+  @ColorInt fun getGalleryIconColor(): Int
+
+  @ColorInt fun getCameraIconColor(): Int
+
+  @NightMode fun getNightModeValue(): Int
+}
